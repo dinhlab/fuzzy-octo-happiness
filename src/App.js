ce529d0a9e4737877187108423628f393564a744
dinh <119478119+dinh292@users.noreply.github.com>
2023-02-21 16:33:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import { useState } from 'react'
+import TaskList from './components/TaskList'
+import AddTaskFrom from './components/AddTaskForm'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 'task_1', title: 'Learn JavaScript', status: 0 },
+    { id: 'task_2', title: 'Code a To-do List', status: 1 }
+  ])
+  const [showIncomplete, setShowIncomplete] = useState(false)
+  const [newTask, setNewTask] = useState('New task')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0
+      }
+      setTasks([...tasks, task])
+      setNewTask('')
+    }
+  }
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value)
+  }
+  const setTaskStatus = (taskId, status) => {
+    setTasks(tasks.map(task => {
+      if (taskId === task.id) {
+        return { ...task, status: status ? 1 : 0 }
+      }
+      return task
+    }))
+  }
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header title='To-do List' subTitle='Get things done' />
+      <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete} />
+      <AddTaskFrom newTask={newTask} handleSubmit={handleSubmit} handleInputChange={handleInputChange} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header ({ title, subTitle }) {
+  return (
+    <h1 className='title'>
+      {title}
+      <span>{subTitle}</span>
+    </h1>
+  )
+}
+
+export default Header
