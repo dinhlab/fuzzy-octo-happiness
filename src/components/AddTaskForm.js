ce181a1a1079ce695ec61d043c1619992f69b7d2
Đình <119478119+dinh292@users.noreply.github.com>
2023-03-01 22:55:16 +0700

    Update AddTaskForm.js

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,7 +2,7 @@ function AddTaskFrom ({ newTask, handleSubmit, handleInputChange }) {
   return (
     <form onSubmit={handleSubmit} className='form'>
       <label htmlFor='newitem'>Add to the todo list</label>
-      <input type='text' id='newitem' value={newTask} onChange={handleInputChange} />
+      <input placeholder='Add new task' type='text' id='newitem' value={newTask} onChange={handleInputChange} />
       <button type='submit'>Add Item</button>
     </form>
   )
